4a56c8a1b98b36be5e7e4db79fe65845ef222831
Daniel Bergman <daniel.bergman96@hotmail.com>
2022-11-11 18:12:30 +0200

    Implemented UpdateTroopRoster partially

---
+++ UniformParties/Helpers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Settlements;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.ObjectSystem;
+
+namespace UniformParties {
+    internal class Helpers {
+        public static PartyTemplateObject FindPartyTemplate(string partyTemplateId) {
+            var partyTemplate = MBObjectManager.Instance.GetObject<PartyTemplateObject>(partyTemplateId);
+
+            if (partyTemplate == null) {
+                InformationManager.DisplayMessage(new InformationMessage("partyTemplate could not be found. Returning 'looters_template'."));
+                partyTemplate = MBObjectManager.Instance.GetObject<PartyTemplateObject>("looters_template");
+            }
+
+            return partyTemplate;
+        }
+
+        public static Kingdom GetRandomKingdomFaction() {
+            return GetKingdomFactions().GetRandomElementInefficiently();
+        }
+
+        public static List<Kingdom> GetKingdomFactions() {
+            return Kingdom.All.ToList();
+        }
+
+        public static Settlement GetRandomSettlement() {
+            return GetSettlements().GetRandomElementInefficiently();
+        }
+
+        public static List<Settlement> GetSettlements() {
+            return Settlement.FindAll(x => !x.IsHideout).ToList();
+        }
+
+        public static List<Settlement> GetSettlementsOfKingdom(Kingdom kingdom) {
+            return Settlement.FindAll(x => !x.IsHideout && x.MapFaction == kingdom).ToList();
+        }
+
+        public static List<Settlement> GetWalledSettlementsOfKingdom(Kingdom kingdom) {
+            return Settlement.FindAll(x => !x.IsHideout && x.MapFaction == kingdom && !x.IsVillage).ToList();
+        }
+
+        public static void Message(String message) {
+            InformationManager.DisplayMessage(new InformationMessage(message));
+        }
+    }
+}
--- UniformParties/UniformPartiesBehavior.cs
+++ UniformParties/UniformPartiesBehavior.cs
@@ -11,26 +11,51 @@ using TaleWorlds.CampaignSystem.Settlements;
 
 namespace UniformParties {
     internal class UniformPartiesBehavior : CampaignBehaviorBase {
+
+        private List<TroopRosterElement> removeList = new List<TroopRosterElement>();
+        private List<TroopRosterElement> addList = new List<TroopRosterElement>();
+
         public override void RegisterEvents() {
             CampaignEvents.DailyTickPartyEvent.AddNonSerializedListener(this, new Action<MobileParty>(OnDailyTickParty));
-            CampaignEvents.DailyTickSettlementEvent.AddNonSerializedListener(this, new Action<Settlement>(OnDailyTickSettlement));
             CampaignEvents.MapEventStarted.AddNonSerializedListener(this, new Action<MapEvent, PartyBase, PartyBase>(OnMapEventStarted));
         }
 
         private void OnDailyTickParty(MobileParty mobileParty) {
-            // Update TroopRoster
+            UpdateTroopRoster(mobileParty.MemberRoster, mobileParty.LeaderHero);
         }
 
-        private void OnDailyTickSettlement(Settlement settlement) {
-            // Update TroopRoster
-        }
 
         private void OnMapEventStarted(MapEvent mapEvent, PartyBase partyBase1, PartyBase partyBase2) {
-            // Update TroopRoster
+            if (mapEvent.IsFieldBattle || mapEvent.IsSiegeAssault || mapEvent.IsSiegeOutside) {
+                Helpers.Message("MapEvent conditions passed");
+                UpdateTroopRoster(partyBase1.MemberRoster, partyBase1.LeaderHero);
+                UpdateTroopRoster(partyBase2.MemberRoster, partyBase2.LeaderHero);
+            }
         }
 
-        private void UpdateTroopRoster(TroopRoster troopRoster) {
+        private void UpdateTroopRoster(TroopRoster troopRoster, Hero leaderHero) {
+            removeList.Clear();
+            addList.Clear();
 
+            foreach (TroopRosterElement troop in troopRoster.GetTroopRoster()) {
+                CharacterObject character = troop.Character;
+
+                // if culture is same continue
+                if (character.Culture == leaderHero.Culture) continue;
+
+                // add to removable troops list
+                removeList.Add(troop);
+
+                // for loop with trooprosterelement.Number
+                for (int i = 0; i < troop.Number; i++) {
+                    // Get troop tier
+                    int tier = character.Tier;
+                    // pick random new unit from list
+                    // add to addList
+                }
+            }
+            // add all units that are in add troops list
+            // remove all units that are in removeable troops list
         }
 
         public override void SyncData(IDataStore dataStore) {}
