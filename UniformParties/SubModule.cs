c1764d1b1c8b33e9ccd06a441f669ff2fa0873e8
Daniel Bergman <daniel.bergman96@hotmail.com>
2022-11-11 16:43:21 +0200

    First commit

---
+++ UniformParties/SubModule.cs
@@ -0,0 +1,18 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace UniformParties {
+    public class SubModule : MBSubModuleBase {
+        protected override void OnSubModuleLoad() {
+            base.OnSubModuleLoad();
+        }
+
+        protected override void InitializeGameStarter(Game game, IGameStarter starterObject) {
+            base.InitializeGameStarter(game, starterObject);
+            if (starterObject is CampaignGameStarter starter) {
+                starter.AddBehavior(new UniformPartiesBehavior());
+            }
+        }
+    }
+}
---
+++ UniformParties/UniformPartiesBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.MapEvents;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Roster;
+using TaleWorlds.CampaignSystem.Settlements;
+
+namespace UniformParties {
+    internal class UniformPartiesBehavior : CampaignBehaviorBase {
+        public override void RegisterEvents() {
+            CampaignEvents.DailyTickPartyEvent.AddNonSerializedListener(this, new Action<MobileParty>(OnDailyTickParty));
+            CampaignEvents.DailyTickSettlementEvent.AddNonSerializedListener(this, new Action<Settlement>(OnDailyTickSettlement));
+            CampaignEvents.MapEventStarted.AddNonSerializedListener(this, new Action<MapEvent, PartyBase, PartyBase>(OnMapEventStarted));
+        }
+
+        private void OnDailyTickParty(MobileParty mobileParty) {
+            // Update TroopRoster
+        }
+
+        private void OnDailyTickSettlement(Settlement settlement) {
+            // Update TroopRoster
+        }
+
+        private void OnMapEventStarted(MapEvent mapEvent, PartyBase partyBase1, PartyBase partyBase2) {
+            // Update TroopRoster
+        }
+
+        private void UpdateTroopRoster(TroopRoster troopRoster) {
+
+        }
+
+        public override void SyncData(IDataStore dataStore) {}
+    }
+}
