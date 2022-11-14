96d33b4c27215703a437e181184f01f221c1ce72
Daniel Bergman <daniel.bergman96@hotmail.com>
2022-11-14 12:19:19 +0200

    Moved troops to their own class

--- UniformParties/FactionTroops.cs
+++ UniformParties/FactionTroops.cs
@@ -9,51 +9,12 @@ using TaleWorlds.ObjectSystem;
 namespace UniformParties {
     internal class FactionTroops {
         public CultureObject Culture { get; private set; }
-        public CharacterObject RegularRecruit { get; private set; }
-        public CharacterObject NobleRecruit { get; private set; }
-
-        public List<CharacterObject>[] RegularTiers { get; private set; } = (from i in Enumerable.Range(0, 7)
-                                                                             select new List<CharacterObject>()).ToArray();
-        public List<CharacterObject>[] NobleTiers { get; private set; } = (from i in Enumerable.Range(0, 7)
-                                                                           select new List<CharacterObject>()).ToArray();
-        public List<CharacterObject> NobleTroops { get; private set; } = new List<CharacterObject>();
-        public List<CharacterObject> RegularTroops { get; private set; } = new List<CharacterObject>();
+        public Troops troops { get; private set; }
         public List<CharacterObject> AllowedTroops { get; private set; } = new List<CharacterObject>();
         
-
         public FactionTroops(string cultureId) {
             Culture = MBObjectManager.Instance.GetObject<CultureObject>(cultureId);
-            var basicTroops = GetCultureBasicTroops(cultureId);
-            RegularRecruit = basicTroops[0];
-            NobleRecruit = basicTroops[1];
-            PopulateListsByTier();
-            //PrintArrayContents();
-        }
-
-        private void PopulateListsByTier() {
-            GetTroop(RegularRecruit, RegularTiers, false);
-            GetTroop(NobleRecruit, NobleTiers, true);
-        }
-
-        private void GetTroop(CharacterObject character, List<CharacterObject>[] listToAddTo, bool noble) {
-            if (character == null) {
-                Helpers.Message("character was null");
-                return;
-            }
-
-            int troopTier = character.Tier;
-            listToAddTo[troopTier - 1].Add(character);
-
-            if (noble) NobleTroops.Add(character);
-            else RegularTroops.Add(character);
-
-            AllowedTroops.Add(character);
-
-            if (character.UpgradeTargets.Length > 0) {
-                for (int i = 0; i < character.UpgradeTargets.Length; i++) {
-                    GetTroop(character.UpgradeTargets[i], listToAddTo, noble);
-                }
-            }
+            troops = new Troops();
         }
 
         public static FactionTroops Find(CultureObject culture, List<FactionTroops> factionTroopsList) {
@@ -79,41 +40,6 @@ namespace UniformParties {
             basicCharacters[1] = culture.EliteBasicTroop;
 
             return basicCharacters;
-        }
-
-        public void PrintArrayContents() {
-            /*
-            Helpers.Message("\nRegular troops: ");
-
-            foreach (var tierList in RegularTiers) {
-                string message = "";
-
-                foreach (var character in tierList) {
-                    message += $" {character.StringId} ";
-                }
-                Helpers.Message(message);
-            }
-
-            Helpers.Message("\nNoble troops: ");
-
-            foreach (var tierList in NobleTiers) {
-                string message = "";
-
-                foreach (var character in tierList) {
-                    message += $" {character.StringId} ";
-                }
-                Helpers.Message(message);
-            }
-
-            Helpers.Message("\nAll noble troops: ");
-            foreach (var troop in AllNobleTroops) {
-                Helpers.Message(troop.ToString());
-            }
-            */
-            Helpers.Message("\nAllowed troops: ");
-            foreach (var troop in AllowedTroops) {
-                Helpers.Message(troop.ToString());
-            }
         }
     }
 }
---
+++ UniformParties/Troops.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.ObjectSystem;
+
+namespace UniformParties {
+    internal class Troops {
+        public List<CharacterObject>[] RegularTiers { get; private set; } = (from i in Enumerable.Range(0, 7)
+                                                                             select new List<CharacterObject>()).ToArray();
+        public List<CharacterObject>[] NobleTiers { get; private set; } = (from i in Enumerable.Range(0, 7)
+                                                                           select new List<CharacterObject>()).ToArray();
+
+        public Troops(List<CharacterObject> regularRecruitTroops, List<CharacterObject> nobleRecruitTroops) {
+            PopulateListsByTier(regularRecruitTroops, nobleRecruitTroops);
+            //PrintArrayContents();
+        }
+
+        private void PopulateListsByTier(List<CharacterObject> regularRecruitTroops, List<CharacterObject> nobleRecruitTroops) {
+            foreach (var character in regularRecruitTroops) {
+                GetTroop(character, false);
+            }
+
+            foreach (var character in nobleRecruitTroops) {
+                GetTroop(character, true);
+            }
+        }
+
+        private void GetTroop(CharacterObject character, bool noble) {
+            if (character == null) {
+                Helpers.Message("character was null");
+                return;
+            }
+
+            int troopTier = character.Tier;
+
+            if (noble) NobleTiers[troopTier - 1].Add(character);
+            else RegularTiers[troopTier - 1].Add(character);
+
+            if (character.UpgradeTargets.Length > 0) {
+                for (int i = 0; i < character.UpgradeTargets.Length; i++) {
+                    GetTroop(character.UpgradeTargets[i], noble);
+                }
+            }
+        }
+    }
+}
