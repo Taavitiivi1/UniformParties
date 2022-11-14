7c48cf35f0929d1ccca714a54196525737279dba
Daniel Bergman <daniel.bergman96@hotmail.com>
2022-11-14 17:52:19 +0200

    Mod now works properly

---
+++ UniformParties/FactionTroops.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.ObjectSystem;
+
+namespace UniformParties {
+    internal class FactionTroops {
+        /// <summary>
+        /// Represents the string name of the kingdom or minor faction, which is used to find the correct FactionTroops class.
+        /// For kingdoms it is cultureId and for minor factions it is clan name
+        /// TODO: Find a better way to do this?
+        /// </summary>
+        public string FactionId { get; private set; }
+        public Troops AllowedTroops { get; private set; }
+        public Clan? Clan { get; private set; }
+        public CultureObject? Culture { get; private set; }
+
+        public List<CharacterObject> RegularRecruits { get; private set; } = new List<CharacterObject>();
+        public List<CharacterObject> NobleRecruits { get; private set; } = new List<CharacterObject>();
+
+        public FactionTroops(string factionId, Clan clan, CultureObject culture) {
+            FactionId = factionId;
+            Clan = clan;
+            Culture = culture;
+
+            if (Culture != null) GetKingdomRecruits(Culture);
+            else if (Clan != null) GetMinorClanRecruits(Clan);
+
+            AllowedTroops = new Troops(RegularRecruits, NobleRecruits);
+        }
+
+        public static FactionTroops Find(string factionId, List<FactionTroops> searchList) {
+            foreach (var factionTroops in searchList) {
+                if (factionTroops.FactionId == factionId) return factionTroops;
+            }
+
+            Helpers.Message("FactionTroops could not be found. Returned null");
+            return null;
+        }
+
+        private void GetKingdomRecruits(CultureObject culture) {
+            if (culture == null) {
+                Helpers.Message("culture was null");
+                return;
+            }
+
+            RegularRecruits.Add(culture.BasicTroop);
+            NobleRecruits.Add(culture.EliteBasicTroop);
+        }
+
+        private void GetMinorClanRecruits(Clan clan) {
+            if (clan == null) {
+                Helpers.Message("clan was null");
+                return;
+            }
+
+            RegularRecruits.Add(clan.BasicTroop);
+        }
+
+        public void Print() {
+            Helpers.Message("\n" + FactionId);
+            foreach (var troop in AllowedTroops.AllTroops) {
+                Helpers.Message(troop.StringId);
+            }
+        }
+    }
+}
--- UniformParties/UniformPartiesBehavior.cs
+++ UniformParties/UniformPartiesBehavior.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.CharacterDevelopment;
@@ -18,7 +19,7 @@ using TaleWorlds.ObjectSystem;
 namespace UniformParties {
     internal class UniformPartiesBehavior : CampaignBehaviorBase {
 
-        public static List<FactionTroopsBase> FactionTroopsList = new List<FactionTroopsBase>();
+        public static List<FactionTroops> FactionTroopsList = new List<FactionTroops>();
 
         public override void RegisterEvents() {
             CampaignEvents.OnAfterSessionLaunchedEvent.AddNonSerializedListener(this, new Action<CampaignGameStarter>(OnAfterSessionLaunched));
@@ -32,62 +33,96 @@ namespace UniformParties {
 
         private void OnAfterSessionLaunched(CampaignGameStarter starter) {
             FactionTroopsList = PopulateFactionTroops();
+            foreach (var f in FactionTroopsList) {
+                f.Print();
+            }
         }
 
         private void UpdateTroopRoster(MobileParty mobileParty, CharacterObject troop, int count) {
-            if (mobileParty == null || mobileParty.LeaderHero == null || mobileParty.LeaderHero.Culture == null || troop == null) return;
+            if (mobileParty == null || mobileParty.LeaderHero == null || mobileParty.LeaderHero.Culture == null || troop == null || mobileParty.IsMainParty) return;
 
             var troopRoster = mobileParty.MemberRoster;
-            var leaderHero = mobileParty.LeaderHero;
-            var leaderCulture = leaderHero.Culture;
             var troopCulture = troop.Culture;
-            var troopTier = troop.Tier; // If tier does not exist, find the next closest one
-            var factionTroopsInstance = FindFactionTroops(); FactionTroopsBase.Find(leaderCulture);
+            // If tier does not exist, find the next closest one
 
-            if (FactionTroopsBase.AllFactionTroopsList.Contains(mobileParty.))
+            FactionTroops factionTroopsInstance;
 
-            if (factionTroopsInstance.AllowedTroops.AllTroops.Contains(troop) || troopRoster == null || mobileParty.IsMainParty || troopCulture.StringId == "neutral_culture") return;
+            if (mobileParty.ActualClan.IsMinorFaction) factionTroopsInstance = FactionTroops.Find(mobileParty.ActualClan.StringId, FactionTroopsList);
+            else factionTroopsInstance = FactionTroops.Find(mobileParty.ActualClan.Kingdom.Culture.StringId, FactionTroopsList);
+
+            if (factionTroopsInstance.AllowedTroops.AllTroops.Contains(troop) || troopRoster == null) return;
 
             List<CharacterObject> troopsOfTier = GetTroopsOfTier(factionTroopsInstance, troop);
 
+            string message = "";
+
             for (int i = 0; i < count; i++) {
-                CharacterObject randomCharacterObject = troopsOfTier[MBRandom.RandomInt(0, troopsOfTier.Count - 1)];
+                CharacterObject randomCharacterObject = randomCharacterObject = troopsOfTier[MBRandom.RandomInt(0, troopsOfTier.Count - 1)];
                 mobileParty.AddElementToMemberRoster(randomCharacterObject, 1);
-                Helpers.Message($"{randomCharacterObject} added to party.");
+                message += $"{randomCharacterObject},  ";
             }
 
+            Helpers.Message(mobileParty.LeaderHero.ToString());
+            Helpers.Message(mobileParty.LeaderHero.Culture.ToString());
+            Helpers.Message(message + "added to party.");
+
             troopRoster.RemoveTroop(troop, count);
-            Helpers.Message($"{count} {troop} removed from party.");
+            Helpers.Message($"{count} {troop} removed from party.\n");
         }
 
-        private List<CharacterObject> GetTroopsOfTier(FactionTroopsBase factionTroops, CharacterObject troop) {
-            if (factionTroops.NobleRecruits.Contains(troop)) return factionTroops.AllowedTroops.NobleTiers[troop.Tier - 1];
-            else return factionTroops.AllowedTroops.RegularTiers[troop.Tier - 1];
+        private List<CharacterObject> GetTroopsOfTier(FactionTroops factionTroops, CharacterObject troop) {
+            int tier = troop.Tier;
+            var nobleRecruits = factionTroops.NobleRecruits;
+            var nobleTiers = factionTroops.AllowedTroops.NobleTiers;
+            var regularTiers = factionTroops.AllowedTroops.RegularTiers;
+
+            // TODO: If tier is empty, find another tier
+
+
+            if (nobleRecruits.Contains(troop)) {
+                return FindBest(nobleTiers, tier);
+            } 
+            else return FindBest(regularTiers, tier);
         }
 
         public override void SyncData(IDataStore dataStore) {}
 
-        private List<FactionTroopsBase> PopulateFactionTroops() {
-            var factionTroops = new List<FactionTroopsBase>();
+        private List<FactionTroops> PopulateFactionTroops() {
+            var factionTroops = new List<FactionTroops>();
 
             foreach (var kingdom in Kingdom.All) {
                 if (kingdom.IsKingdomFaction) {
-                    CultureObject culture = kingdom.Culture;
-                    if (culture.StringId == "empire_w" || culture.StringId == "empire_s") continue;
-                    factionTroops.Add(new KingdomFactionTroops(culture));
-                    Helpers.Message(culture.StringId);
+                    string stringId = kingdom.Culture.StringId;
+                    if (stringId == "empire_w" || stringId == "empire_s") continue;
+                    factionTroops.Add(new FactionTroops(stringId, null, kingdom.Culture));
+                    //Helpers.Message(stringId);
                 }
             }
 
             foreach (var clan in Clan.All) {
                 if (clan.IsMinorFaction && !clan.IsBanditFaction && !(clan == Clan.PlayerClan)) {
                     string stringId = clan.StringId;
-                    Helpers.Message(stringId);
-                    factionTroops.Add(new MinorClanFactionTroops(clan));
+                    //Helpers.Message(stringId);
+                    factionTroops.Add(new FactionTroops(stringId, clan, null));
                 }
             }
 
             return factionTroops;
+        }
+
+        private List<CharacterObject> FindBest(List<CharacterObject>[] tiers, int tier) {
+            int tiersCount = tiers.Length;
+
+            for (int i = tier; i < tiersCount; i++) { // First we always find higher tier
+                if (tiers[i].Count > 0) return tiers[i];
+            }
+
+            for (int i = tier - 1; i > 0; i--) { // If we reached max tier and could not find an non-empty tier, go downwards
+                if (tiers[i].Count > 0) return tiers[i];
+            }
+
+            Helpers.Message("Next best tier could not be found. Returning tiers[0]");
+            return tiers[0];
         }
     }
 }
